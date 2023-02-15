34c1e8c076f38e8e1f7ef3a90c3e8327c4755d2d
Dinuka Perera <dinuka.20210433@iit.ac.lk>
2023-02-16 01:28:09 +0530

    Try on spyder

---
+++ firstRunOnSpyder.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 22:44:00 2023
+
+@author: dinukaperera
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('HAYL Historical Data.csv')
+print(df.head())
