47c1397050e35e94959c53466dc4d0be1cb662db
Dinuka Perera <dinuka.20210433@iit.ac.lk>
2023-03-18 12:52:33 +0530

    Implementation

--- app.py
+++ app.py
@@ -1,12 +1,60 @@
-#Create flask app
+# Import required libraries
+import pickle
+from flask import Flask, request, jsonify
+import pandas as pd
+
+# Load the pickled models
+model1 = pickle.load(open('model1.pkl', 'rb'))
+model2 = pickle.load(open('model2.pkl', 'rb'))
+
+# Load the data frame from a CSV file
+data_frame = pd.read_csv('data.csv')
+
+# Create a Flask app
 app = Flask(__name__)
 
-#Load the pickle model
-model = pickle.load(open("Model for General Motors.pkl","rb"))
+# Define a function for making prediction 1
+def predict1(data):
+    # Preprocess the data as required
+    # Select the required features from the data frame
+    features = ['feature1', 'feature2', 'feature3']
+    data = data_frame.loc[data_frame['id'] == data['id'], features].values
+    # Make prediction using model 1
+    pred = model1.predict(data)
+    # Return the prediction
+    return pred
 
-@app.route("/")
-def Home():
-    return render_template("")
+# Define a function for making prediction 2
+def predict2(data):
+    # Preprocess the data as required
+    # Select the required features from the data frame
+    features = ['feature4', 'feature5', 'feature6']
+    data = data_frame.loc[data_frame['id'] == data['id'], features].values
+    # Make prediction using model 2
+    pred = model2.predict(data)
+    # Return the prediction
+    return pred
 
-@app.route("/predict", method =["POST"])
-def predict():
+# Define an endpoint for receiving data and making predictions
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    # Get the data from the request
+    data = request.get_json(force=True)
+    # Check user input to determine which model to use
+    if data['model'] == 'model1':
+        prediction = predict1(data)
+    elif data['model'] == 'model2':
+        prediction = predict2(data)
+    else:
+        return jsonify({'error': 'Invalid model selected!'})
+    # Return the prediction as a JSON object
+    return jsonify({'prediction': prediction.tolist()})
+
+# Define a home page
+@app.route('/')
+def home():
+    return 'Welcome to my multi-pickle ML implementation using Flask!'
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
